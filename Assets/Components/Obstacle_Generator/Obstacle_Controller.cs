884c1da13b5f3d60d6b307ab0154a47b5217d180
Corentin Vernet <vernet.corentin4@gmail>
2026-03-07 18:36:31 +0100

    Système de segment
    création/déstruction et déplacement des segments

--- Assets/Components/Obstacle_Generator/Obstacle_Controller.cs
+++ Assets/Components/Obstacle_Generator/Obstacle_Controller.cs
@@ -6,11 +6,12 @@ public class Obstacle_Controller : MonoBehaviour
     [Header("Parameter")]
     [SerializeField, Tooltip("Speed: m/s")] private float _translationSpeed = 1f;
     [SerializeField] private int _activeCountSegment = 10;
+    [SerializeField] private int _behindCountSegment = 1;
 
     [Header("Component")]
     [SerializeField] private Segment_Controller[] _segmentPool;
 
-    private List<Segment_Controller> _instanciedSegment = new();
+    private List<Segment_Controller> _instancedSegment = new();
 
     private void Start()
     {
@@ -19,7 +20,44 @@ public class Obstacle_Controller : MonoBehaviour
 
     private void Update()
     {
-        
+        foreach (var segment in _instancedSegment) // pour déplacer les segments vers l'arrière
+        {
+            segment.transform.Translate(Vector3.back * _translationSpeed * Time.deltaTime);
+        }
+
+        UpdateSegment();
+    }
+
+    private void UpdateSegment()
+    {
+        List<Segment_Controller> segmentBack = new();
+
+        foreach (var segment in _instancedSegment)
+        {
+            if (segment.IsBehindPlayer()) // si un segment est derrière a la liste des segmentBack
+            {
+                segmentBack.Add(segment);
+            }
+        }
+
+        if (segmentBack.Count > _behindCountSegment) // si il y a plus de 2 segment, on delete le premier
+        {
+            int segmentDeleteCount = segmentBack.Count - _behindCountSegment;
+
+            for (int i = 0; i < segmentDeleteCount; i++)
+            {
+                var segmentDelete = segmentBack[i];
+                _instancedSegment.Remove(segmentDelete);
+                Destroy(segmentDelete.gameObject);
+            }
+        }
+
+        int missingSegmentCount = _activeCountSegment - _instancedSegment.Count;
+        for (int i = 0; i < missingSegmentCount; i++)
+        {
+            var segment = AddSegment(LastActiveSegment().EndAnchor);
+            _instancedSegment.Add(segment);
+        }
     }
 
     private void AddBaseSegment(Vector3 position)
@@ -29,12 +67,12 @@ public class Obstacle_Controller : MonoBehaviour
             if (i == 0) // si aucun segment on en instantie un a "baseSegment" et on continue la boucle
             {
                 var baseSegment = AddSegment(transform.position);
-                _instanciedSegment.Add(baseSegment);
+                _instancedSegment.Add(baseSegment);
                 continue;
             }
 
-            var segment = AddSegment(LastSegment().EndAnchor); // si il y a deja des segments on les instantie la pos de l'ancre du dernier segment
-            _instanciedSegment.Add(segment); // et on l'ajoute a la liste
+            var segment = AddSegment(LastActiveSegment().EndAnchor); // si il y a deja des segments on les instantie la pos de l'ancre du dernier segment
+            _instancedSegment.Add(segment); // et on l'ajoute a la liste
         }
     }
 
@@ -52,8 +90,8 @@ public class Obstacle_Controller : MonoBehaviour
         return segment;
     }
 
-    private Segment_Controller LastSegment() // on return le dernier segment
+    private Segment_Controller LastActiveSegment() // on return le dernier segment
     {
-        return _instanciedSegment[_instanciedSegment.Count -1];
+        return _instancedSegment[_instancedSegment.Count -1];
     }
 }
--- Assets/Components/Obstacle_Generator/Segment_Controller.cs
+++ Assets/Components/Obstacle_Generator/Segment_Controller.cs
@@ -5,4 +5,9 @@ public class Segment_Controller : MonoBehaviour
     [SerializeField] private Transform _endAnchor;
 
     public Vector3 EndAnchor => _endAnchor.position; // "=>" (c'est un "getter") qui sert a rendre publique une variable privé
+
+    public bool IsBehindPlayer()
+    {
+        return EndAnchor.z < 0;
+    }
 }
